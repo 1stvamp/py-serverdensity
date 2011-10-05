34fc1ca8bcd02d7c5a72516740040972d07ebd41
Wesley Mason <wes@1stvamp.org>
2011-10-05 11:10:45 +0100

    Initial files

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Installer for py-serverdensity
+"""
+
+import os
+cwd = os.path.dirname(__file__)
+__version__ = open(os.path.join(cwd, 'serverdensity', 'version.txt'), 'r').read().strip()
+
+try:
+        from setuptools import setup, find_packages
+except ImportError:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, find_packages
+setup(
+    name='serverdensity',
+    description='Python ServerDensity.com API wrapper',
+    version=__version__,
+    author='Wes Mason',
+    author_email='wes@boxedice.com',
+    url='http://github.com/boxedice/py-serverdensity',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=open('requirements.txt').readlines(),
+    package_data={'serverdensity': ['version.txt']},
+    include_package_data=True,
+    license='BSD'
+)
