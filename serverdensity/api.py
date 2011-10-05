772ef6a3b4cbcc81efd44af7811e6a03a2f84711
Wesley Mason <wes@1stvamp.org>
2011-10-05 15:22:11 +0100

    Initial working API wrapper

---
+++ serverdensity/api.py
@@ -0,0 +1,98 @@
+import requests
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+GETS = {
+    'alerts': (
+        'getHistory', 'getLast', 'getOpen', 'getOpenNotified', 'list',
+    ),
+    'services': (
+        'list',
+    ),
+    'devices': (
+        'getByGroup', 'getByHostname', 'getById', 'getByIp', 'getByName',
+        'list', 'listGroups', 'rename',
+    ),
+    'metrics': (
+        'getLatest', 'getRange', 'list',
+    ),
+    'mongo': (
+        'getMaster', 'getReplicaSet',
+    ),
+    'processes': (
+        'getByTime', 'getRange',
+    ),
+    'users': (
+        'getById', 'delete',
+    ),
+}
+
+POSTS = {
+    'alerts': (
+        'pause', 'resume',
+    ),
+    'devices': (
+        'add', 'addGroup', 'delete',
+    ),
+    'metrics': (
+        'postback',
+    ),
+    'users': (
+        'add',
+    ),
+}
+
+API_VERSION = '1.3'
+BASE_URL = 'https://api.serverdensity.com/%(version)s/%(section)s/%(method)s'
+
+class SDApi(object):
+    """Lightweight ServerDensity.com API wrapper
+    """
+    def __init__(self, account, username, password, api_key, name=None,
+                 gets=GETS, posts=POSTS, base_url=BASE_URL,
+                 api_version=API_VERSION):
+        self._account = account
+        self._username = username
+        self._password = password
+        self._api_key = api_key
+        self._name = name
+        self._gets = gets
+        self._posts = posts
+        self._base_url = base_url
+        self._api_version = api_version
+
+    def _request(self, method, params={}):
+        if 'apiKey' not in params:
+            params['apiKey'] = self._api_key
+        if 'account' not in params:
+            params['account'] = self._account
+
+        url = self._base_url % {
+            'version': self._api_version,
+            'section': self._name,
+            'method': method,
+        }
+
+        if method in self._gets[self._name]:
+            r = requests.get(url, params=params, auth=(self._username,
+                self._password))
+        elif method in self._posts[self._name]:
+            r = requests.post(url, params=params, auth=(self._username,
+                self._password))
+        return json.loads(r.content)
+
+    def __getattr__(self, attr_name):
+        if self._name is None:
+            return super(SDApi, self).__self_class__(
+                account=self._account,
+                username=self._username,
+                password=self._password,
+                api_key=self._api_key,
+                name=attr_name
+            )
+        else:
+            def wrapper(*args, **kwargs):
+                return self._request(attr_name, *args, **kwargs)
+            return wrapper
